c95c4831cc6a43d852c984fd9d929530b8afeb2e
Hatem Abdelsattar Elsayed <hatemabdelsattar@gmail.com>
2025-03-04 17:14:34 +0200

    Migrate to SCSS and update project configuration
    - Convert styles from CSS to SCSS
    - Add PrimeIcons dependency
    - Update Tailwind configuration to include more file types
    - Adjust import statements for Tailwind and PrimeIcons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    // ...
-    darkMode: "selector",
-    content: ["./src/**/*.{html,js,ts}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [require("tailwindcss-primeui")],
+  // ...
+  darkMode: "selector",
+  content: ["./src/**/*.{html,ts,scss,css}", "./index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("tailwindcss-primeui")],
 };
