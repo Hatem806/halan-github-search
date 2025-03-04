aa54ebd3117697cdaac92250209fa147660fcdd8
Hatem Abdelsattar Elsayed <hatemabdelsattar@gmail.com>
2025-03-04 11:56:30 +0200

    Initialize PrimeNG and Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // ...
+    darkMode: "selector",
+    content: ["./src/**/*.{html,js,ts}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [require("tailwindcss-primeui")],
+};
