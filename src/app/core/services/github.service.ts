6bcecc45f24036970449b8cc150aa8c4059c474b
Hatem Abdelsattar Elsayed <hatemabdelsattar@gmail.com>
2025-03-04 20:23:24 +0200

    Implement GitHub user search method

--- src/app/core/services/github.service.ts
+++ src/app/core/services/github.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GithubService {
+  private http = inject(HttpClient);
 
-  constructor() { }
+  private API_URL = 'https://api.github.com';
+
+  searchUsers(
+    query: string,
+    page: number = 1,
+    perPage: number = 10
+  ): Observable<any> {
+    return this.http.get<any>(
+      `${this.API_URL}/search/users?q=${query}&page=${page}&per_page=${perPage}`
+    );
+  }
 }
