a678b1dd9e17049268b55d65ef007e865b9421e6
Hatem Abdelsattar Elsayed <hatemabdelsattar@gmail.com>
2025-03-05 16:38:07 +0200

    Remove Tailwind configuration file

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  // ...
-  darkMode: "selector",
-  content: ["./src/**/*.{html,ts,scss,css}", "./index.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("tailwindcss-primeui")],
-};
